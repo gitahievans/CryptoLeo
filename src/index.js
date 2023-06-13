7bcec0738e628716b493c99b620a3aabd62de33d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-13 16:18:56 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Trending from './pages/Trending';
+import Saved from './pages/Saved';
+import Crypto from './pages/Crypto';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children:  [
+      {
+        path: "/",
+        element: <Crypto />
+      },
+      {
+        path: "/trending",
+        element: <Trending />
+      },
+      {
+        path: "/saved",
+        element: <Saved />
+      }
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
