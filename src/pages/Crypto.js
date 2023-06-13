c1494eb7cecc4c2a71efd30aa4465d58fc9122db
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-13 16:17:18 +0300

    Added pages

---
+++ src/pages/Crypto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TableComponent from '../components/TableComponent'
+
+const Crypto = () => {
+  return (
+    <section className='w-[80%] h-full flex flex-col mt-16 mb-24 relative'>
+      <TableComponent />
+    </section>
+  )
+}
+
+export default Crypto
