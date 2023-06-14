b60773c8bb2a2af6b57a52dc0e9c744f192317ae
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-14 20:24:18 +0300

    Updated CryptoContext component

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -9,10 +9,13 @@ export const CryptoProvider = ({children}) => {
     const [searchData, setSearchData] = useState()
     const [coinSearch, setCoinSearch] = useState("")
 
+    const [currency, setCurrency] = useState("usd")
+    const [sortBy, setSortBy] = useState("market_cap_desc")
+
     const getCryptoData = async () => {
         try{
 
-            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinSearch}&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
+            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
             ).then(res => res.json()).then(json => json) 
 
             setCryptoData(data)
@@ -35,10 +38,21 @@ export const CryptoProvider = ({children}) => {
 
     useLayoutEffect(() => {
         getCryptoData()
-    },[coinSearch])
+    },[coinSearch, currency, sortBy])
 
     return(
-        <CryptoContext.Provider value={{cryptoData, searchData, getSearchResult, setCoinSearch,setSearchData}}>
+        <CryptoContext.Provider value={{
+            cryptoData, 
+            searchData, 
+            getSearchResult, 
+            setCoinSearch,
+            setSearchData, 
+            currency,
+            setCurrency,
+            sortBy,
+            setSortBy
+            }}
+            >
             {children}
         </CryptoContext.Provider>
     )
