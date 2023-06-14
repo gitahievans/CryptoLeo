215c784b6f658bd1297cb5880ce843fe09b4e3c8
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-14 14:30:19 +0300

    Updated Crypto page

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import TableComponent from '../components/TableComponent'
+import Filters from '../components/Filters'
 
 const Crypto = () => {
   return (
     <section className='w-[80%] h-full flex flex-col mt-16 mb-24 relative'>
+      <Filters />
       <TableComponent />
     </section>
   )
