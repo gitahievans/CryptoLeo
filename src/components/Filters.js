0cc3ece0b90b181fe3e1e122e19ed99fb0928f58
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-14 17:59:31 +0300

    Updated Filters component

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Search from './Search'
+import submitIcon from '../assets/submit-icon.svg'
 
 const Filters = () => {
   return (
     <div className='w-full h-12 border-2 border-gray-100 rounded-lg flex items-center justify-between relative'>
         <Search />
-        <div>currency</div>
+        <div className='flex mr-7'>
+            <form className='relative flex items-center font-nunito mr-12'>
+              <label htmlFor='currency' className='relative flex justify-center items-center mr-2 font-bold'> currency</label>
+                <input className='w-16 rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0
+                border border-transparent focus:border-cyan leading-4'
+                  type='text'name='currency' placeholder='usd'/>
+                <button type='submit' className='ml-1 cursor-pointer'>
+                  <img src={submitIcon} alt='submit' className='w-full h-auto' />
+                </button>
+            </form>
+        </div>
         <div>sorting</div>
     </div>
   )
