5b94f7ae935f9591e30740eea13ea139ef727967
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-14 17:58:26 +0300

    Updated CryptoContext

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -6,11 +6,13 @@ export const CryptoContext = createContext({})
 // create the provider component
 export const CryptoProvider = ({children}) => {
     const [cryptoData, setCryptoData] = useState()
+    const [searchData, setSearchData] = useState()
+    const [coinSearch, setCoinSearch] = useState("")
 
     const getCryptoData = async () => {
         try{
 
-            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
+            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinSearch}&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
             ).then(res => res.json()).then(json => json) 
 
             setCryptoData(data)
@@ -19,12 +21,24 @@ export const CryptoProvider = ({children}) => {
         }
     }
 
+    const getSearchResult = async (query) => {
+        try{
+
+            const data = await fetch(`https://api.coingecko.com/api/v3/search?query=${query}`
+            ).then(res => res.json()).then(json => json) 
+
+            setSearchData(data.coins)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
     useLayoutEffect(() => {
         getCryptoData()
-    },[])
+    },[coinSearch])
 
     return(
-        <CryptoContext.Provider value={{cryptoData}}>
+        <CryptoContext.Provider value={{cryptoData, searchData, getSearchResult, setCoinSearch,setSearchData}}>
             {children}
         </CryptoContext.Provider>
     )
