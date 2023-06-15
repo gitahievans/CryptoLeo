e89d72f0b0e915c3900ec02f43406cb753f67f0c
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-15 17:56:56 +0300

    Updated CryptoContext

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -7,11 +7,14 @@ export const CryptoContext = createContext({})
 export const CryptoProvider = ({children}) => {
     const [cryptoData, setCryptoData] = useState()
     const [searchData, setSearchData] = useState()
+    const [coinData, setCoinData] = useState()
     const [coinSearch, setCoinSearch] = useState("")
 
     const [currency, setCurrency] = useState("usd")
     const [sortBy, setSortBy] = useState("market_cap_desc")
     const [page, setPage] = useState(1)
+    const [totalPages, setTotalPages] = useState(250)
+    const [perPage, setPerPage] = useState(10)
 
     const getCryptoData = async () => {
 
@@ -19,18 +22,32 @@ export const CryptoProvider = ({children}) => {
 
             const data = await fetch(`https://api.coingecko.com/api/v3/coins/list`
             ).then(res => res.json()).then(json => json) 
-
             
+            setTotalPages(data.length)
         }catch(error){
             console.log(error)
         }
 
         try{
 
-            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
+            const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
             ).then(res => res.json()).then(json => json) 
 
             setCryptoData(data)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    const getCoinData = async (coinId) => {
+
+        try{
+
+            const data = await fetch(`
+            https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=true&sparkline=false`
+            ).then(res => res.json()).then(json => json) 
+
+            setCoinData(data)
         }catch(error){
             console.log(error)
         }
@@ -48,9 +65,14 @@ export const CryptoProvider = ({children}) => {
         }
     }
 
+    const resetFunction = () => {
+        setPage(1)
+        setCoinSearch("")
+    }
+
     useLayoutEffect(() => {
         getCryptoData()
-    },[coinSearch, currency, sortBy, page])
+    },[coinSearch, currency, sortBy, page, perPage])
 
     return(
         <CryptoContext.Provider value={{
@@ -62,7 +84,12 @@ export const CryptoProvider = ({children}) => {
             currency,
             setCurrency,
             sortBy, setSortBy,
-            page, setPage
+            page, setPage,
+            totalPages,
+            resetFunction,
+            setPerPage,
+            perPage,
+            getCoinData, coinData
             }}
             >
             {children}
