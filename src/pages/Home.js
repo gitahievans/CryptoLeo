ba0a29cfc4613f04c09a0785c3b8ceb53784b08a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-16 18:51:54 +0300

    Updated Trending page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,12 @@ import { Outlet } from 'react-router-dom'
 import Logo from '../components/Logo'
 import Navigation from '../components/Navigation'
 import { CryptoProvider } from '../context/CryptoContext'
+import { TrendingProvider } from '../context/TrendingContext'
 
 const Home = () => {
   return (
-    <CryptoProvider>       
+    <CryptoProvider> 
+    <TrendingProvider>       
       <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white
       font-nunito">
           <div className='w-screen h-screen bg-gray-300 fixed -z-10' />
@@ -14,6 +16,7 @@ const Home = () => {
       <Navigation />
       <Outlet />
       </main>
+    </TrendingProvider>   
     </CryptoProvider>
   )
 }
