9f454cc7e4eb5fcd668e2574a307ad10d73723d9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-16 18:52:29 +0300

    Updated Home

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TrendingContext } from '../context/TrendingContext'
+import TrendingCoin from '../components/TrendingCoin'
 
 const Trending = () => {
+
+  const {trendData} = useContext(TrendingContext)
+
   return (
-    <div>Trending</div>
+    <section className='w-[80%] h-full flex flex-col mt-16 mb-24 relative'>
+      <div className='w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly mt-9 border border-gray-100 rounded'>
+        {
+          trendData && trendData.map(coin =>
+            <TrendingCoin key={coin} data={coin.item} />
+          )
+        }
+      </div>
+    </section>
   )
 }
 
