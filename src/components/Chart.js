0169baef0c18554e9608631c761f0b87a507c1a9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-16 18:50:37 +0300

    Added chart component

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -29,7 +29,7 @@ const ChartComponent = ({data, currency, type}) => {
             <Line type="monotone" dataKey={type} stroke="#25da72" strokeWidth={"1px"} />
             <CartesianGrid stroke="#323232" />
             <XAxis dataKey="date" hide />
-            <YAxis dataKey="prices" hide domain={["auto","auto"]} />
+            <YAxis dataKey={type} hide domain={["auto","auto"]} />
             <Tooltip content={<CustomTooltip />} currency={currency} cursor={false} wrapperStyle={{outline: "none"}} />
             <Legend />
         </LineChart>
@@ -41,13 +41,14 @@ const Chart = ({id}) => {
     const [chartData, setChartData] = useState()
     let {currency} = useContext(CryptoContext)
     const [type, setType] = useState("prices")
+    const [days, setDays] = useState(7)
 
     useLayoutEffect(() => {
     const getChartData = async (id) => {
 
             try{
 
-                const data = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7&interval=daily`
+                const data = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}&interval=daily`
                 ).then(res => res.json()).then(json => json) 
 
                 let convertedData = data[type].map(item => 
@@ -67,15 +68,35 @@ const Chart = ({id}) => {
 
     getChartData(id)
 
-    }, [id, type])
+    }, [id, type, days])
 
     return (
         <div className='w-full h-[60%]'>
             <ChartComponent data={chartData} currency={currency} type={type} />
             <div className='flex'>
-                <button onClick={() => setType("prices")}>Price</button>
-                <button onClick={() => setType("market_caps")}>market caps</button>
-                <button onClick={() => setType("total_volumes")}>total volumes</button>
+                <button className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize
+                ${type === "prices" ? 'bg-cyan text-cyan': 'bg-gray-200 text-gray-100'}`}
+                onClick={() => setType("prices")}>Price</button>
+
+                <button className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize
+                ${type === "market_cap" ? 'bg-cyan text-cyan': 'bg-gray-200 text-gray-100'}`}
+                onClick={() => setType("market_caps")}>market caps</button>
+
+                <button className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize
+                ${type === "total_volumes" ? 'bg-cyan text-cyan': 'bg-gray-200 text-gray-100'}`} 
+                onClick={() => setType("total_volumes")}>total volumes</button>
+
+                <button className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize
+                ${days === 7 ? 'bg-cyan text-cyan': 'bg-gray-200 text-gray-100'}`} 
+                onClick={() => setDays(7)}>7d</button>
+
+                <button className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize
+                ${days === 14 ? 'bg-cyan text-cyan': 'bg-gray-200 text-gray-100'}`} 
+                onClick={() => setDays(14)}>14d</button>
+
+                <button className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize
+                ${days === 30 ? 'bg-cyan text-cyan': 'bg-gray-200 text-gray-100'}`} 
+                onClick={() => setDays(30)}>30d</button>
             </div>
         </div>
     )
