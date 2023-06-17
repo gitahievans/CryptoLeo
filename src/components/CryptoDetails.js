258e44b1c50a025ea45f0f59e7165e3795b380cc
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-17 15:11:13 +0300

    Updated CryptoDetails component

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -331,7 +331,14 @@ const CryptoDetails = () => {
                     
                     </div>
                 </div>
-                : null
+                :
+                
+                <div className='w-full min-h-[60vh] h-full flex justify-center items-center'>
+                        
+                <div className='w-8 h-8 border-4 border-cyan rounded-full border-b-gray-200 animate-spin
+                ' role='status' />
+                <span className='ml-2'>Please wait...</span>
+            </div>
             }
         </div>
     </div>,
