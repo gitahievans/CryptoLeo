7b1e069ea617cb0e40307ee3b0c5c93d328e7eac
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-01 20:20:31 +0300

    Updated Crypto page

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import TableComponent from '../components/TableComponent'
-import Filters from '../components/Filters'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Filters from "../components/Filters";
+import TableComponent from "../components/TableComponent";
 
 const Crypto = () => {
   return (
-    <section className='w-[80%] h-full flex flex-col mt-16 mb-24 relative'>
+    <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
       <Filters />
       <TableComponent />
       <Outlet />
     </section>
-  )
-}
+  );
+};
 
-export default Crypto
+export default Crypto;
