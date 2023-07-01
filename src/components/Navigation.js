902bbdb7d0c73ffa97cc8add9205b2adc422f7d9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-01 20:27:38 +0300

    Updated Navigation Component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,40 +1,63 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-  return (
-    <nav className='w-[40%] mt-16 flex justify-around align-middle border border-cyan rounded'>
-        <NavLink to='/' end className={
-            ({isActive}) => {
-                return `w-full text-base text-center font-nunito m-2.5 ${isActive ? 'bg-cyan text-gray-300' :
-            'bg-gray-200 text-gray-100 hover:text-cyan active:bg-cyan active:text-gray-300 '}
-            border-0 cursor-pointer rounded capitalize font-semibold`
-            }
-        }>
-            Crypto
-        </NavLink>
+return (
+    <nav
+    className="w-[40%] mt-16 flex justify-around align-middle
+    border border-cyan rounded-lg
+    "
+    >
+    <NavLink
+        to="/"
+        end
+        className={({ isActive }) => {
+        return `w-full text-base text-center font-nunito m-2.5
 
-        <NavLink to='/trending' className={
-            ({isActive}) => {
-                return `w-full text-base text-center font-nunito m-2.5 ${isActive ? 'bg-cyan text-gray-300' :
-            'bg-gray-200 text-gray-100 hover:text-cyan active:bg-cyan active:text-gray-300 '}
-            border-0 cursor-pointer rounded capitalize font-semibold`
-            }
-        }>
-            Trending
-        </NavLink>
-
-        <NavLink to='/saved' className={
-            ({isActive}) => {
-                return `w-full text-base text-center font-nunito m-2.5 ${isActive ? 'bg-cyan text-gray-300' :
-            'bg-gray-200 text-gray-100 hover:text-cyan active:bg-cyan active:text-gray-300 '}
-            border-0 cursor-pointer rounded capitalize font-semibold`
-            }
-        }>
-            Saved
-        </NavLink>
-    </nav>
-  )
+${
+isActive
+    ? "bg-cyan text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
 }
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+    >
+        Crypto
+    </NavLink>
 
-export default Navigation
+    <NavLink
+        to="/trending"
+        className={({ isActive }) => {
+        return `w-full text-base text-center font-nunito m-2.5
+
+${
+isActive
+    ? "bg-cyan text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
+}
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+    >
+        trending
+    </NavLink>
+
+    <NavLink
+        to="/saved"
+        className={({ isActive }) => {
+        return `w-full text-base text-center font-nunito m-2.5
+
+${
+isActive
+    ? "bg-cyan text-gray-300"
+    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
+}
+    border-0 cursor-pointer rounded capitalize font-semibold`;
+        }}
+    >
+        saved
+    </NavLink>
+    </nav>
+);
+};
+
+export default Navigation;
