8753a916eb34afd9df76b7cb04d8f2c3ca4b676f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-01 20:28:20 +0300

    Updated Logo Component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logoSvg from "../assets/logo.svg"
+import React from "react";
+import { Link } from "react-router-dom";
+import logoSvg from "../assets/logo.svg";
 
 const Logo = () => {
   return (
-    <Link to='/' className='absolute top-[1.5rem] left-[1.5rem] [text-decoration:none] text-lg text-cyan
-    flex items-center'>
-        <img src={logoSvg} alt='CryptoLeo' />
-        <span>CryptoLeo</span>
+    <Link
+      to="/"
+      className="
+    absolute top-[1.5rem] left-[1.5rem] [text-decoration:none]
+text-lg text-cyan flex items-center
+    "
+    >
+      <img src={logoSvg} alt="CryptoBucks" />
+      <span>CryptoLeo</span>
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
