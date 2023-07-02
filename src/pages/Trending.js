6df3447fe786784b75c611e04750668bdc41867d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-02 12:05:07 +0300

    Updated Trending page

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -8,14 +8,14 @@ const Trending = () => {
   const { trendData, resetTrendingResult } = useContext(TrendingContext);
 
   return (
-    <section className="w-[80%] h-full flex flex-col sm:w-full mt-16 mb-24 relative">
+    <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
       <div className="w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly  border border-gray-100 rounded">
         {trendData &&
           trendData.map((coin) => (
             <TrendingCoin key={data.coin_id} data={coin.item} />
           ))}
         <button
-          className="w-[2rem] sm:w-full ml-4 hover:scale-110 transition-all transition-ease
+          className="w-[2rem] ml-4 hover:scale-110 transition-all transition-ease
         absolute right-0 -top-10
         "
           onClick={resetTrendingResult}
