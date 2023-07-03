8d8c5ef36d494e158b30142e5f77f11b5d4a727e
munene-m <munenenjue18@gmail.com>
2023-07-03 21:09:57 +0300

    screensize was not defined

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
             <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
             {/* DoNotDelete This is for refactoring during development */}
             <p className="fixed top-4 right-4 font-semibold text-red text-lg">
-              {screenSize}
+              {}
             </p>
             <Logo />
             <Navigation />
